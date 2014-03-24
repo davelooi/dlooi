21a3987d3ca1982371eabf7892ba97bde40c6d96
Dave Looi <davlooi@hotmail.com>
2014-03-25 01:47:27 +0800

    dynamic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,39 +5,13 @@ module ApplicationHelper
     "dLooi"
   end
 
-  def site_url
-    if Rails.env.production?
-      # Place your production URL in the quotes below
-      "http://dlooi.herokuapp.com/"
-    else
-      # Our dev & test URL
-      "http://localhost:3000"
-    end
+  def page_title(separator = " | ")
+    [content_for(:title), site_name].compact.join(separator)
   end
 
-  def meta_author
-    # Change the value below between the quotes.
-    "Dave Looi"
-  end
-
-  def meta_description
-    # Change the value below between the quotes.
-    "row row row sampan.."
-  end
-
-  def meta_keywords
-    # Change the value below between the quotes.
-    "davelooi dave looi sampan sampansaurus boatzilla"
-  end
-
-  # Returns the full title on a per-page basis.
-  # No need to change any of this we set page_title and site_name elsewhere.
-  def full_title(page_title)
-    if page_title.empty?
-      site_name
-    else
-      "#{page_title} | #{site_name}"
-    end
+  def page_heading(title)
+    content_for(:title){ title }
+    content_tag(:h1, title)
   end
 
 end
