5f08732bbb4818e50183de0f4a26a8c007f84440
Dave Looi <davlooi@hotmail.com>
2014-03-27 22:35:06 +0800

    add association between user and skill_category; modify skills partial; bold education title

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_one :user_summary
   has_many :educations
   has_many :skills
+  has_many :skill_categories, -> { uniq }, :through => :skills
 
   validates :name, :uid, :email, presence: true
 
